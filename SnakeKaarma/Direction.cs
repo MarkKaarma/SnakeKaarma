9036d474aac3f88f6e412b41a18462c99b353178
MarkKaarma <mark.kaarma@gmail.com>
2020-05-25 15:37:49 +0300

    New-old project

---
+++ SnakeKaarma/Direction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeKaarma
+{
+    enum Direction // Создаем фундамент для управление "стрелочками"
+    {
+        LEFT,
+        RIGHT,
+        UP,
+        DOWN
+    }
+}
